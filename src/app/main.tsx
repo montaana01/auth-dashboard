0838cdd3708ede1991b09957a007059635545eef
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2026-02-04 18:21:31 +0300

    refactor: update schema of application && install react router

---
+++ src/app/main.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/auth" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+);
