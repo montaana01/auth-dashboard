80bd1a8a524b9de864a16c65b7d177abf60c1b53
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2026-02-07 02:18:29 +0300

    refactor: add basic rtk implementations on auth form

--- src/pages/auth/components/AuthForm.tsx
+++ src/pages/auth/components/AuthForm.tsx
@@ -1,17 +1,39 @@
 import type {AuthFormProps} from "@/app/types/AuthFormTypes.ts";
+import {useDispatch, useSelector} from "react-redux";
+
+import {login} from "@/shared/store/auth/authSlice.ts";
+import type {RootState} from "@/app/store.ts";
 
 //TODO: add form input and submit handling
 
-export const AuthForm = ({ type }: AuthFormProps ) => {
+export const AuthForm = ({type}: AuthFormProps) => {
+  const isAuth = useSelector((state: RootState) => state.auth.isAuth)
+  const dispatch = useDispatch()
+
   if (!type) return (<>
     <h1>Form type is not provided to Auth Form</h1>
   </>);
 
   if (type === 'login') return (<>
     <h1>Login Form</h1>
+    {!isAuth && (
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(login())}
+      >
+        Login
+      </button>)}
   </>);
 
   if (type === 'register') return (<>
     <h1>Register Form</h1>
+    {!isAuth && (
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(login())}
+      >
+        Register
+      </button>
+    )}
   </>);
 }
